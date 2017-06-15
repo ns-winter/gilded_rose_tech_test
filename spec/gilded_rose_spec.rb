9dfd556e58ec0955448868acc355e8729f231b76
Nigel Said <nigelsaid@gmail.com>
2017-06-15 17:14:43 +0100

    adds Backstage passes tests

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,8 +3,8 @@ require 'item'
 
 describe GildedRose do
 
-  context "Standard Item" do
-    describe "#update_quality" do
+  describe "#update_quality" do
+    context "Standard Item" do
       before :all do
         items = [
           Item.new("+5 Dexterity Vest", 10, 20),
@@ -30,30 +30,64 @@ describe GildedRose do
       it "value can't go negative" do
         expect(@gilded_rose.items[2].quality).to eq 0
       end
+    end
 
-      context "Aged Brie" do
-        describe "#update_quality" do
-          before :all do
-            items = [
-              Item.new("Aged Brie", 2, 0)
-            ]
-            @gilded_rose = GildedRose.new(items)
-            @gilded_rose.update_quality
-          end
+    context "Aged Brie" do
+      before :all do
+        items = [
+          Item.new("Aged Brie", 2, 0),
+          Item.new("Aged Brie", -1, 49)
+        ]
+        @gilded_rose = GildedRose.new(items)
+        @gilded_rose.update_quality
+      end
 
-          it "increases in value over time" do
-            expect(@gilded_rose.items[0].quality).to eq 1
-          end
+      it "increases in value over time" do
+        expect(@gilded_rose.items[0].quality).to eq 1
+      end
 
-          it "increases in value at double rate when sell by date is passed" do
-            @gilded_rose.update_quality
-            @gilded_rose.update_quality
-            expect(@gilded_rose.items[0].quality).to eq 4
-          end
+      it "increases in value at double rate when sell by date is passed" do
+        @gilded_rose.update_quality
+        @gilded_rose.update_quality
+        expect(@gilded_rose.items[0].quality).to eq 4
+      end
+
+      it "quality doesn't exceed 50" do
+        expect(@gilded_rose.items[1].quality).to eq 50
+      end
+    end
+
+    context "Backstage passes" do
+      before :all do
+        items = [
+          Item.new("Backstage passes to a TAFKAL80ETC concert", 11, 46),
+          Item.new("Backstage passes to a TAFKAL80ETC concert", 2, 20)
+        ]
+        @gilded_rose = GildedRose.new(items)
+        @gilded_rose.update_quality
+      end
+
+      it "increases in value over time at a standard rate with >= 10 days" do
+        expect(@gilded_rose.items[0].quality).to eq 47
+      end
+
+      it "increases in value at double rate within 6-10 days" do
+        @gilded_rose.update_quality
+        expect(@gilded_rose.items[0].quality).to eq 49
+      end
 
 
+      it "increases in value over time at triple rate with >= 5 days" do
+        expect(@gilded_rose.items[1].quality).to eq 26
+      end
 
-        end
+      it "drops to 0 value after sell-by-date" do
+        @gilded_rose.update_quality
+        expect(@gilded_rose.items[1].quality).to eq 0
+      end
+
+      it "quality doesn't exceed 50" do
+        expect(@gilded_rose.items[0].quality).to eq 50
       end
     end
   end
