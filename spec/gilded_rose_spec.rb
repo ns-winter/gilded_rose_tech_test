e834eacde3727dbad13917145dedfd29e96baba9
Nigel Said <nigelsaid@gmail.com>
2017-06-15 16:35:48 +0100

    adds more tests for standard items

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -6,7 +6,11 @@ describe GildedRose do
   context "Standard Item" do
     describe "#update_quality" do
       before :each do
-        items = [Item.new("+5 Dexterity Vest", 10, 20)]
+        items = [
+          Item.new("+5 Dexterity Vest", 10, 20),
+          Item.new("Boar Meat", -1, 10),
+          Item.new("Shoddy dagger", 0, 0)
+        ]
         @gilded_rose = GildedRose.new(items)
         @gilded_rose.update_quality
       end
@@ -18,6 +22,16 @@ describe GildedRose do
       it "decreases in value at standard rate" do
         expect(@gilded_rose.items[0].quality).to eq 19
       end
+
+      it "decreases in value at double rate when sell-by-date is passed" do
+        expect(@gilded_rose.items[1].quality).to eq 8
+      end
+
+      it "value can't go negative" do
+        expect(@gilded_rose.items[2].quality).to eq 0
+      end
+
+
     end
   end
 end
