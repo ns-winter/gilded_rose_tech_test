978595336383f924a03aca9e38269323953da4ee
Nigel Said <nigelsaid@gmail.com>
2017-06-15 16:53:45 +0100

    adds 'Aged Brie' tests

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -5,7 +5,7 @@ describe GildedRose do
 
   context "Standard Item" do
     describe "#update_quality" do
-      before :each do
+      before :all do
         items = [
           Item.new("+5 Dexterity Vest", 10, 20),
           Item.new("Boar Meat", -1, 10),
@@ -31,7 +31,30 @@ describe GildedRose do
         expect(@gilded_rose.items[2].quality).to eq 0
       end
 
+      context "Aged Brie" do
+        describe "#update_quality" do
+          before :all do
+            items = [
+              Item.new("Aged Brie", 2, 0)
+            ]
+            @gilded_rose = GildedRose.new(items)
+            @gilded_rose.update_quality
+          end
 
+          it "increases in value over time" do
+            expect(@gilded_rose.items[0].quality).to eq 1
+          end
+
+          it "increases in value at double rate when sell by date is passed" do
+            @gilded_rose.update_quality
+            @gilded_rose.update_quality
+            expect(@gilded_rose.items[0].quality).to eq 4
+          end
+
+
+
+        end
+      end
     end
   end
 end
